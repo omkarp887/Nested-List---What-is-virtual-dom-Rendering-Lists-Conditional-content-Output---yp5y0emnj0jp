46973dd57907177c7d45837ace6daa71e17f392b
Omkar Pradip Patil <88555432+omkarp887@users.noreply.github.com>
2022-09-28 12:05:15 +0530

    Create state.js

---
+++ src/components/state.js
@@ -0,0 +1,34 @@
+import React, { Component, useState } from "react";
+import CityComponent from'./city'
+
+const StateComponent=(props)=>{
+    const [changeStates,setStates]=useState(-1)
+    const openCloseStates=(e)=>{
+        if(changeStates==-1){
+            setStates(e.target.value-1)
+        }
+        else{
+            setStates(-1)
+        }
+        e.stopPropagation()
+    }
+    
+     return(
+                <ol>
+                    {
+                        props.arrayStates.map((eState,index)=>{
+                                return <li id={`state${index+1}`} value={index+1}onClick={openCloseStates}>{eState.name}
+                                    {changeStates==index&&(
+                                       <CityComponent arrayStates1={[...props.arrayStates]} indexState={index} changedState={changeStates}/>
+                                    )
+
+                                    }
+                                    
+                                </li>
+                        })
+                    }
+                </ol>
+            )
+}
+
+export default StateComponent;
