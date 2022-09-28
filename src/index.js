1e8ececc681bea88e75e6cd6545db7161f3d7d53
Omkar Pradip Patil <88555432+omkarp887@users.noreply.github.com>
2022-09-28 12:12:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import City from "./components/city";
+import State from "./components/state";
+import Town from "./components/town";
 
 ReactDOM.render(<App />, document.getElementById("root"));
